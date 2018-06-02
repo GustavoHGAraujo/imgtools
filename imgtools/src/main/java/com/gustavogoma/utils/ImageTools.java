21152bec23cd9bb06643eeec2af5095efeee2837
Gustavo Henrique Gomes de Araújo <gustavo.hg.araujo@gmail.com>
2018-06-02 14:07:22 -0300

    Adapt to better use in Kotlin codes

--- imgtools/src/main/java/com/gustavogoma/utils/ImageTools.java
+++ imgtools/src/main/java/com/gustavogoma/utils/ImageTools.java
@@ -1,4 +1,4 @@
-package com.gustavogoma.utils.imgtools;
+package com.gustavogoma.utils;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -9,7 +9,8 @@ import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.os.AsyncTask;
-import android.util.Log;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
@@ -22,173 +23,186 @@ import java.net.URL;
 import static android.view.View.GONE;
 
 /**
- * @author GustavoHGAraujo
+ * @author Gustavo Araújo
  */
 
 public class ImageTools extends AsyncTask<Void, Void, Bitmap> {
 
-    private static final String TAG = ImageTools.class.getSimpleName();
-
     /**
      * Decode the byte array to a Bitmap.
+     *
      * @param byteArray Bitmap byte array
      * @return Bitmap
      */
-    public static Bitmap byteArray2Bitmap(byte[] byteArray){
-        if (byteArray != null) {
-            return BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
-        } else {
-            return null;
-        }
+    public static Bitmap byteArrayToBitmap(@NonNull byte[] byteArray) {
+        return BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
     }
 
     /**
      * Encode the Bitmap to a byte array.
+     *
      * @param bitmap Bitmap byte array
      * @return byte[] Encoded bitmap
      */
-    public static byte[] bitmap2ByteArray(Bitmap bitmap){
-        if (bitmap != null) {
-            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
-            return stream.toByteArray();
-        } else {
-            return null;
-        }
+    public static byte[] bitmapToByteArray(@NonNull Bitmap bitmap) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        return stream.toByteArray();
     }
 
     /**
      * Downloads a image through its url.
-     * @param imgUrl Image URL
-     * @param onImageDownloadListener Listener called when the download is done.
+     *
+     * @param imageUrl Image URL
+     * @return ImageTools instance. It contains the events of the download.
      */
-    public static void download(String imgUrl, OnImageDownloadListener onImageDownloadListener) {
+    public static ImageTools download(@NonNull String imageUrl) {
         ImageTools imgTools = new ImageTools();
-        imgTools.imgUrl = imgUrl;
-        imgTools.l = onImageDownloadListener;
+        imgTools.imageUrl = imageUrl;
 
-        imgTools.execute();
-        Log.d(TAG, "download: IMG_URL: " + imgUrl);
+        return imgTools;
     }
 
     /**
      * Set the image into ImageView and, if the ProgressBar isn't null, set its visibility as GONE.
-     * @param imgView ImageView where the image will be shown
+     *
+     * @param imageView     ImageView where the image will be shown
      * @param progressBar ProgressBar that, if not null, will be hidden
-     * @param bitmap Image to be shown
+     * @param bitmap      Image to be shown
      */
-    public static void setImage(ImageView imgView, ProgressBar progressBar, Bitmap bitmap) {
-        imgView.setImageBitmap(bitmap);
-        imgView.setAlpha(1f);
-        imgView.setBackground(null);
-        imgView.setPadding(0, 0, 0, 0);
+    public static void setImage(@NonNull ImageView imageView,
+                                @NonNull Bitmap bitmap,
+                                @Nullable ProgressBar progressBar) {
+        imageView.setImageBitmap(bitmap);
+        imageView.setAlpha(1f);
+        imageView.setBackground(null);
+        imageView.setPadding(0, 0, 0, 0);
         if (progressBar != null) progressBar.setVisibility(GONE);
     }
 
     /**
      * Returns a squared image
+     *
      * @param bmp Image to be cropped
      * @return Bitmap Cropped image
      */
-    private static Bitmap getSquareBitmap(Bitmap bmp) {
-        if (bmp == null) return null;
+    public static Bitmap getSquareBitmap(@NonNull Bitmap bmp) {
+        final int x;
+        final int y;
+        final int width;
+        final int height;
 
-        Bitmap output;
-        if (bmp.getWidth() >= bmp.getHeight()){
-            output = Bitmap.createBitmap(
-                    bmp,
-                    bmp.getWidth()/2 - bmp.getHeight()/2,
-                    0,
-                    bmp.getHeight(),
-                    bmp.getHeight()
-            );
-
-        }else{
-            output = Bitmap.createBitmap(
-                    bmp,
-                    0,
-                    bmp.getHeight()/2 - bmp.getWidth()/2,
-                    bmp.getWidth(),
-                    bmp.getWidth()
-            );
+        if (bmp.getWidth() >= bmp.getHeight()) {
+            x = bmp.getWidth() / 2 - bmp.getHeight() / 2;
+            y = 0;
+            width = bmp.getHeight();
+            height = bmp.getHeight();
+        } else {
+            x = 0;
+            y = bmp.getHeight() / 2 - bmp.getWidth() / 2;
+            width = bmp.getWidth();
+            height = bmp.getWidth();
         }
 
-        return output;
+        return Bitmap.createBitmap(bmp, x, y, width, height);
     }
 
     /**
      * Returns a circled image
+     *
      * @param bitmap Image to be cropped
      * @return Bitmap Cropped image
      */
-    public static Bitmap getCircularBitmap(Bitmap bitmap){
+    public static Bitmap getCircularBitmap(@NonNull Bitmap bitmap) {
         return getRoundedCornerBitmap(getSquareBitmap(bitmap), bitmap.getWidth());
     }
 
     /**
      * * Returns a rounded corner image
+     *
      * @param bitmap Image to be cropped
      * @param pixels Corner radius
      * @return Bitmap Cropped image
      */
-    public static Bitmap getRoundedCornerBitmap(Bitmap bitmap, int pixels) {
-        Log.v(TAG, "getRoundedCornerBitmap");
-
-        if (bitmap == null) return null;
-
-        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap
-                .getHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(output);
+    public static Bitmap getRoundedCornerBitmap(@NonNull Bitmap bitmap, int pixels) {
         final int color = 0xff424242;
+        final float roundPx = pixels;
         final Paint paint = new Paint();
         final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
         final RectF rectF = new RectF(rect);
-        final float roundPx = pixels;
+
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                                            bitmap.getHeight(),
+                                            Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
         paint.setAntiAlias(true);
         canvas.drawARGB(0, 0, 0, 0);
         paint.setColor(color);
         canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
         paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
         canvas.drawBitmap(bitmap, rect, rect, paint);
+
         return output;
     }
 
-    private String imgUrl;
-    private OnImageDownloadListener l;
+    private String imageUrl;
+    private OnSuccessListener onSuccessListener;
+    private OnFailedListener onFailedListener;
 
     private ImageTools() {
-        this.imgUrl = "";
+        this.imageUrl = "";
+        this.onSuccessListener = new OnSuccessListener() {
+            @Override
+            public void onImageDownloaded(Bitmap bitmap) {
+            }
+        };
+        this.onFailedListener = new OnFailedListener() {
+            @Override
+            public void onImageDownloadFailed(Exception e) {
+            }
+        };
     }
 
     @Override
     protected Bitmap doInBackground(Void... voids) {
-        Log.v(TAG, "doInBackground");
-        if (imgUrl == null) return null;
+        if (imageUrl == null) return null;
 
-        Bitmap bmp = null;
-        try{
-            URL url = new URL(imgUrl);
-            HttpURLConnection con = (HttpURLConnection)url.openConnection();
-            InputStream is = con.getInputStream();
-            bmp = BitmapFactory.decodeStream(is);
+        Bitmap bitmap = null;
+        try {
+            URL url = new URL(imageUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = connection.getInputStream();
+            bitmap = BitmapFactory.decodeStream(inputStream);
         } catch (IOException e) {
-            Log.e(TAG, "doInBackground", e);
-            if (l != null) l.onImageDownloadFailed(e);
+            this.onFailedListener.onImageDownloadFailed(e);
         }
 
-        return bmp;
+        return bitmap;
     }
 
     @Override
     protected void onPostExecute(Bitmap bmp) {
-        Log.v(TAG, "onPostExecute");
         super.onPostExecute(bmp);
 
-        if (l != null) l.onImageDownloaded(bmp);
+        this.onSuccessListener.onImageDownloaded(bmp);
     }
 
-    public interface OnImageDownloadListener {
+    public ImageTools onSuccess(OnSuccessListener onSuccessListener) {
+        this.onSuccessListener = onSuccessListener;
+        return this;
+    }
+
+    public ImageTools onFailed(OnFailedListener onFailedListener) {
+        this.onFailedListener = onFailedListener;
+        return this;
+    }
+
+    private interface OnSuccessListener {
         void onImageDownloaded(Bitmap bitmap);
+    }
+
+    private interface OnFailedListener {
         void onImageDownloadFailed(Exception e);
     }
 }
